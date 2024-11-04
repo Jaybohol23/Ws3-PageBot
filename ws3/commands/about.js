f24ac38dbeec4e6f33d90e96724ef754892d6bc9
Jaybohol23 <163659582+Jaybohol23@users.noreply.github.com>
2024-11-04 12:19:12 +0800

    Update about.js

--- ws3/commands/about.js
+++ ws3/commands/about.js
@@ -1,11 +1,11 @@
 module.exports = {
-  description: "What is WieAI?",
+  description: "What is Menta AI?",
   async run({ api, send, admin }){
     await send({
       attachment: {
         type: "image",
         payload: {
-          url: "https://i.imgur.com/gw1V46p.jpeg",
+          url: "",
           is_reusable: true
         }
       }
@@ -15,26 +15,26 @@ module.exports = {
         type: "template",
         payload: {
           template_type: "button",
-          text: `🤖 About WieAI:
-WieAI is your friendly, helpful personal assistant.
+          text: `🤖 About Menta AI:
+Menta AI is your friendly, helpful personal assistant.
 
-💭 Why I named WieAI as a name of the page bot because this is dedicated to my girlfriend itself 👸
+💭 Why I named Menta AI as a name of the page bot
 
 ❓ Contact us admins if you experienced/encountered any issue regarding to the bot and I will try to fix it. Thankyou for using me as a personal assistant!`,
           buttons: [
             {
               type: "web_url",
-              url: "https://www.facebook.com/profile.php?id=61566907376981",
+              url: "https://www.facebook.com/profile.php?id=61568288608152",
               title: "Like/Follow our Page"
                 },
             {
               type: "web_url",
-              url: "https://www.facebook.com/kennethfranciscoaceberos",
+              url: "https://www.facebook.com/profile.php?id=61550037082227",
               title: "Contact Admin 1"
                 },
             {
               type: "web_url",
-              url: "https://www.facebook.com/wieginesalpocialechavez",
+              url: "https://www.facebook.com/profile.php?id=61556388598622",
               title: "Contact Admin 2"
                 }
              ]
